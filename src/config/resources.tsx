8407a54dd661d2decc05fc231debd3e5d6f8f07c
Van Chi Hieu <vanchihieu3@gmail.com>
2024-02-09 19:15:33 +0700

    App routes and layout setup

---
+++ src/config/resources.tsx
@@ -0,0 +1,48 @@
+import type { IResourceItem } from "@refinedev/core";
+
+import {
+  DashboardOutlined,
+  ProjectOutlined,
+  ShopOutlined,
+} from "@ant-design/icons";
+
+export const resources: IResourceItem[] = [
+  /**
+   * A resource in Refine performs these actions:
+   * list -> get all records (Read)
+   * show -> get a single record (Read)
+   * create -> create a record (Create)
+   * edit -> update a record (Update)
+   * delete -> delete a record (Delete)
+   * meta -> meta information about the resource
+   */
+  {
+    name: "dashboard",
+    list: "/",
+    meta: {
+      label: "Dashboard",
+      icon: <DashboardOutlined />,
+    },
+  },
+  {
+    name: "companies",
+    list: "/companies",
+    show: "/companies/:id",
+    create: "/companies/new",
+    edit: "/companies/edit/:id",
+    meta: {
+      label: "Companies",
+      icon: <ShopOutlined />,
+    },
+  },
+  {
+    name: "tasks",
+    list: "/tasks",
+    create: "/tasks/new",
+    edit: "/tasks/edit/:id",
+    meta: {
+      label: "Tasks",
+      icon: <ProjectOutlined />,
+    },
+  },
+];
---
+++ src/graphql/types.ts
@@ -0,0 +1,36 @@
+import type * as Types from "./schema.types";
+
+export type UpdateUserMutationVariables = Types.Exact<{
+  input: Types.UpdateOneUserInput;
+}>;
+
+export type UpdateUserMutation = {
+  updateOneUser: Pick<
+    Types.User,
+    "id" | "name" | "avatarUrl" | "email" | "phone" | "jobTitle"
+  >;
+};
+
+export type UsersSelectQueryVariables = Types.Exact<{
+  filter: Types.UserFilter;
+  sorting?: Types.InputMaybe<Array<Types.UserSort> | Types.UserSort>;
+  paging: Types.OffsetPaging;
+}>;
+
+export type UsersSelectQuery = {
+  users: Pick<Types.UserConnection, "totalCount"> & {
+    nodes: Array<Pick<Types.User, "id" | "name" | "avatarUrl">>;
+  };
+};
+
+export type TaskStagesSelectQueryVariables = Types.Exact<{
+  filter: Types.TaskStageFilter;
+  sorting?: Types.InputMaybe<Array<Types.TaskStageSort> | Types.TaskStageSort>;
+  paging: Types.OffsetPaging;
+}>;
+
+export type TaskStagesSelectQuery = {
+  taskStages: Pick<Types.TaskStageConnection, "totalCount"> & {
+    nodes: Array<Pick<Types.TaskStage, "id" | "title">>;
+  };
+};
